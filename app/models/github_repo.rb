748e63e9eaca07e72948f7db79cfa89522c54d60
github username <github email address>
2018-12-02 20:11:50 -0500

    Done.

--- app/models/github_repo.rb
+++ app/models/github_repo.rb
@@ -2,6 +2,9 @@ class GithubRepo
 
   attr_reader :name, :url
 
+# sets key 'name' to @name
+# sets key 'html_url' to @url
+
   def initialize(hash)
     @name = hash["name"]
     @url = hash["html_url"]
